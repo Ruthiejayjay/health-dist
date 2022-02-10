0292781a28906f44a738648618dcc44ef547d7f4
Ruth James <84787216+Ruthiejayjay@users.noreply.github.com>
2022-02-10 19:00:19 +0100

    first commit

---
+++ _nuxt/static/1644515540/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fmedical","\u002Foncology","\u002Fmonitoring","\u002Fultrasound"]})
---
+++ _nuxt/static/1644515540/oncology/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/oncology", {data:[{}],fetch:{},mutations:void 0});
